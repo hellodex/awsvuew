ad5c680755713d218b6124012ec30ee596364a70
huangfupo <1561117958@qq.com>
2025-03-21 13:39:33 +0800

    feat: aws demo

---
+++ src/hooks/useConnectWallet.ts
@@ -0,0 +1,46 @@
+import { watch } from 'vue'
+import { ElMessageBox, ElMessage } from 'element-plus'
+
+import { useAppKitAccount, useAppKit, useDisconnect } from '@reown/appkit/vue'
+import { useGlobalStore } from '@/stores/global'
+
+export function useConnectWallet() {
+  const globalStore = useGlobalStore()
+  const modal = useAppKit()
+  const appkitAccount = useAppKitAccount()
+  const { disconnect } = useDisconnect()
+
+  const connectWallet = () => {
+    modal.open()
+  }
+
+  const disconnectWallet = () => {
+    ElMessageBox.confirm('是否断开钱包连接？', '提示', {
+      confirmButtonText: '确认',
+      cancelButtonText: '取消',
+      type: 'info'
+    })
+      .then(() => {
+        ElMessage.error('断开成功')
+        disconnect()
+      })
+      .catch(() => {})
+  }
+
+  watch([() => appkitAccount.value.address, () => appkitAccount.value.isConnected, () => appkitAccount.value.caipAddress], () => {
+    console.log('account updated:', appkitAccount.value.address)
+    console.log('account updated:', appkitAccount.value.isConnected)
+    console.log('account updated:', appkitAccount.value.caipAddress)
+    console.log(appkitAccount.value.caipAddress?.split(':'))
+    globalStore.setWalletInfo({
+      address:appkitAccount.value.address,
+      isConnected:appkitAccount.value.isConnected,
+      chainId: appkitAccount.value.caipAddress?.split(':')[1],
+    })
+  })
+
+  return {
+    connectWallet,
+    disconnectWallet
+  }
+}
---
+++ src/views/HomeView.vue
@@ -0,0 +1,25 @@
+
+<template>
+  <h1>VITE_API_URL:{{ WEB_URL }}</h1>
+
+  <h1>secret env:{{ secret_env }}</h1>
+  
+  <WalletConnect v-if="!isConnected" />
+  <WalletDisconnected v-if="isConnected"/>
+</template>
+
+<script setup lang="ts">
+import { computed } from 'vue'
+import WalletConnect from '@/components/WalletConnect.vue'
+import WalletDisconnected from '@/components/WalletDisconnected.vue'
+import { secret } from '@aws-amplify/backend';
+import { useGlobalStore } from '@/stores/global'
+
+const globalStore = useGlobalStore()
+const WEB_URL = import.meta.env.VITE_API_URL
+
+const secret_env = secret('VITE_API_URL')
+
+const isConnected = computed(() => globalStore.walletInfo.isConnected)
+</script>
+
